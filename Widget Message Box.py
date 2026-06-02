8372fd89494e30f98bab4ed5309bd249e4c8ac86
FranciscoFerreira2012 <shark11714@sharkcoders.pt>
2026-06-02 18:38:58 +0100

    Add files via upload

---
+++ Widget Message Box.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+
+messagebox.showinfo('Title', 'Information')
+messagebox.showwarning('Title', 'Warning')
+messagebox.showerror('Title', 'Error')
+
+root.mainloop()
